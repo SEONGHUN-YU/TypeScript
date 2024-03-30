4e30916d5dcaa0f453c49bc0ee36834e7ddec5b4
SEONGHUN-YU <versprechen@protonmail.ch>
2024-03-31 05:46:31 +0900

    Study Type

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,26 @@
+// JS
+// var str = 'hello'
+
+// TS 문자열, 숫자, 진위값(boolean)
+let str: string = 'hello'
+let num: number = 10
+let show: boolean = true
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3]
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10]
+let items: number[] = [4, 5, 6]
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 100]
+
+// TS 객체
+let obj: object = {}
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// }
+let person: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000
+}
