eb75ac12895dbe8b7aece1513ffad3607f612fdd
SEONGHUN-YU <versprechen@protonmail.ch>
2024-04-17 20:08:28 +0900

    Study Utility-Types

---
+++ class-note/13_utility-type.ts
@@ -0,0 +1,74 @@
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    brand: string;
+    stock: number
+}
+
+// 1. 상품 목록을 받아오기 위한 API 함수
+function fetchProducts(): Promise<Product[]> {
+    // ...
+}
+
+// interface ProductDetail {
+//     id: number;
+//     name: string;
+//     price: number
+// }
+
+// 2. 특정 상품의 상세 정보를 나타내기 위한 함수
+type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
+function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>): void {
+
+}
+
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number
+// }
+
+// 3. 특정 상품 정보를 업데이트(갱신)하는 함수
+type UpdateProduct = Partial<Product>
+function updateProductItem(productItem: Partial<Product>): void {
+
+}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+    username: string;
+    email: string;
+    profilePhotoUrl: string
+}
+
+// interface UserProfileUpdate {
+//     username?: string;
+//     email?: string;
+//     profilePhotoUrl?: string
+// }
+
+// #1
+// type UserProfileUpdate = {
+//     username?: UserProfile['username'];
+//     email?: UserProfile['email'];
+//     profilePhotoUrl?: UserProfile['profilePhotoUrl']
+// }
+
+// #2
+type UserProfileUpdate = {
+    [P in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[P]
+}
+
+// #3
+type UserProfileKeys = keyof UserProfile
+type UserProfileUpdate = {
+    [P in UserProfileKeys]?: UserProfile[P]
+}
+
+// #4
+type Subset<T> = {
+    [P in keyof T]?: T[P]
+}
